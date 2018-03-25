f0599762f61462d80042c22cc4d71401ffcb5643
Taylor <tay333@gmail.com>
2018-03-25 15:30:34 -0700

    better now

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,35 @@
 
+#![recursion_limit="128"]
+
 #[macro_use]
 extern crate yew;
 extern crate rand;
 
 use yew::html::*;
+use std::iter;
 
 struct Context {
 }
 
 struct Model {
+    dim: usize,
 }
 
 enum Msg {
     Step,
+    Incr,
+    Decr,
 }
-const DIM: usize = 60;
 
 fn update(_context: &mut Context, model: &mut Model, msg: Msg) {
     match msg {
-        Msg::Step => {}
+        Msg::Step => {},
+        Msg::Incr => {
+            model.dim += 1;
+        },
+        Msg::Decr => {
+            model.dim -= 1;
+        },
     };
 }
 
@@ -26,28 +37,47 @@ fn view(model: &Model) -> Html<Msg> {
     html! {
         <div>
             <header class="app-header",>
-                <h1 class="app-title",>{ "Conway's Game of Life" }</h1>
-            </header>
-            <section class="game-container",>
-                <section class="game-area",>
-                    <table class="grid",>
-                        { for (0..DIM).map(view_row) }
-                    </table>
-                    <div class="game-buttons",>
-                        <button onclick=|_| Msg::Step,>{ "Next Generation" }</button>
+                <section class="section",>
+                    <div class="container",>
+                        <h1 class="title",>{ "Conway's Game of Life" }</h1>
                     </div>
-                </section>
+                </section> 
+            </header>
+            <section class="section",>
+                <div class="container",>
+                    <div class="level",>
+                        <div class="level-item",>
+                            <button class="button", onclick=|_| Msg::Step,>{ "Next Generation" }</button>
+                            <button class="button",  onclick=|_| Msg::Incr,>{ "Zoom In" }</button>
+                            <span class="tag",> { model.dim } </span>
+                            <button class="button", onclick=|_| Msg::Decr,>{ "Zoom Out" }</button>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="section",>
+                <div class="container",>
+                    <div class="level",>
+                        <div class="level-item",>
+                            <table class="grid",>
+                                { for (0..model.dim).zip(iter::repeat(model.dim)).map(view_row) }
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </section>
         </div>
     }
 }
-fn view_row(_idx: usize) -> Html<Msg> {
+
+fn view_row((_idx, dim): (usize, usize)) -> Html<Msg> {
     html! {
         <tr class="row",>
-            { for (0..DIM).map(view_cell) }
+            { for (0..dim).map(view_cell) }
         </tr>
     }
 }
+
 fn view_cell(_idx: usize) -> Html<Msg> {
     html! {
         <td class=("cell", if rand::random() { "living" } else { "dead" } ),</td>
@@ -60,6 +90,7 @@ fn main() {
     let context = Context {
     };
     let model = Model {
+        dim: 20
     };
     app.mount(context, model, update, view);
     yew::run_loop();
