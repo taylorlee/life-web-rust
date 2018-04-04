2b6205f3299f22bcca1b16b6b244f9b1b53a0bb5
Taylor <tay333@gmail.com>
2018-04-04 12:07:22 -0700

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
 #![recursion_limit = "256"]
 
 mod game;
-mod web;
+mod model;
+mod actions;
+mod view;
 
 extern crate rand;
 #[macro_use]
 extern crate yew;
 
+use yew::{initialize, run_loop, html::App, services::interval::IntervalService};
+
 fn main() {
-    web::serve();
+    initialize();
+    let mut app = App::new();
+    let context = model::Context {
+        interval: IntervalService::new(app.sender()),
+    };
+    let model = model::Model {
+        board: game::setup(),
+        speed: 5,
+        clock: 0,
+        job: None,
+        running: false,
+    };
+    app.mount(context, model, actions::update, view::render);
+    run_loop();
 }
--- src/web.rs
+++ src/web.rs
@@ -1,12 +1,9 @@
-
 use std::time::Duration;
 
-use yew;
-use yew::html::*;
-use yew::services::interval::IntervalService;
-use yew::services::Task;
+use yew::{initialize, run_loop, html::{App, Html}, services::{Task, interval::IntervalService}};
 
-use game::*;
+use game::{board_slice, next_generation, setup, Board};
+use view::view;
 
 struct Model {
     board: Board,
@@ -29,22 +26,15 @@ struct Context {
     interval: IntervalService<Msg>,
 }
 
-const LEN: usize = 50;
-const DIM: usize = LEN*2;
-
-type Row = [bool; DIM];
-type Grid = [Row; DIM];
-
-fn new_grid() -> Grid {
-    [[false; DIM]; DIM]
-}
-
 fn cycle_time(speed: u8) -> Duration {
-    Duration::from_millis(500 - 50 * speed as u64)
+    Duration::from_millis(1000 - (100 * (speed as u64)))
 }
 
 fn do_start(context: &mut Context, model: &mut Model) {
-    let handle = context.interval.spawn(cycle_time(model.speed), || Msg::Step);
+    let timeout = cycle_time(model.speed);
+    let handle = context
+        .interval
+        .spawn(timeout, || Msg::Step);
     model.job = Some(Box::new(handle));
     model.running = true;
 }
@@ -97,106 +87,9 @@ fn update(context: &mut Context, model: &mut Model, msg: Msg) {
     };
 }
 
-fn board2grid(board: &Board) -> Grid {
-    let mut rows = new_grid();
-    for rdx in 0..DIM {
-        for cdx in 0..DIM {
-            let x = rdx as isize - LEN as isize;
-            let y = cdx as isize - LEN as isize;
-            rows[rdx][cdx] = board_slice(&board, x, y);
-        }
-    }
-    return rows;
-}
-
-fn view(model: &Model) -> Html<Msg> {
-    let rows = board2grid(&model.board);
-    html! {
-        <div>
-            <section class="section",>
-                <div class="container",>
-                    <div class="level",>
-                        <div class="level-item",>
-                            <h1 class="title",>{ "Game of Life" }</h1>
-                        </div>
-                    </div>
-                </div>
-                <div class="level",></div>
-                <div class="container",>
-                    <div class="level",>
-                        <div class="level-item",>
-                            <div class=("tags","has-addons"),>
-                              <span class="tag",> {"Generation #"}</span>
-                              <span class=("tag","is-primary"),> { model.clock } </span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="level",>
-                        { if model.running { show_pause() } else { show_start() } }
-                    </div>
-                    <div class="level",>
-                        <div class="level-item",>
-                            <div class=("tags","has-addons"),>
-                              <span class="tag",> {"SPEED"} </span>
-                              <span class=("tag","is-primary"),> { model.speed} </span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="level",>
-                        <div class="level-item",>
-                            <button class="button", onclick=|_| Msg::Decr,>{ "Slow Down" }</button>
-                            <button class="button",  onclick=|_| Msg::Incr,>{ "Speed Up" }</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="level",></div>
-                <div class="container",>
-                    <div class="level",>
-                        <div class="level-item",>
-                            <table class="grid",>
-                                { for rows.iter().map(view_row)  }
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </div>
-    }
-}
-
-fn show_start() -> Html<Msg> {
-    html! {
-        <div class="level-item",>
-            <button class="button", onclick=move|_| Msg::Step,>{ "Step" }</button>
-            <button class="button", onclick=move|_| Msg::Start,>{ "Start" }</button>
-            <button class="button", onclick=move|_| Msg::Reset,>{ "Reset" }</button>
-        </div>
-    }
-}
-fn show_pause() -> Html<Msg> {
-    html! {
-        <div class="level-item",>
-            <button class="button", onclick=move|_| Msg::Stop,>{ "Pause" }</button>
-        </div>
-    }
-}
-
-fn view_row(cells: &Row) -> Html<Msg> {
-    html! {
-        <tr class="row",>
-            { for cells.iter().map(view_cell) }
-        </tr>
-    }
-}
-
-fn view_cell(living: &bool) -> Html<Msg> {
-    html! {
-        <td class=("cell", if *living { "living" } else { "dead" } ),</td>
-    }
-}
 
 pub fn serve() {
-    yew::initialize();
+    initialize();
     let mut app = App::new();
     let context = Context {
         interval: IntervalService::new(app.sender()),
@@ -209,5 +102,5 @@ pub fn serve() {
         running: false,
     };
     app.mount(context, model, update, view);
-    yew::run_loop();
+    run_loop();
 }
